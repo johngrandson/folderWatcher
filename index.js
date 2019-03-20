89f5069623e610f7f409c6144422f8281b777c22
João Netto <joaonettopb@hotmail.com>
2019-03-20 01:55:47 -0300

    created progressbar

--- index.js
+++ index.js
@@ -1,149 +1,49 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').load();
-}
+var SocketIOFileUpload = require('socketio-file-upload'),
+    socketio = require('socket.io'),
+    express = require('express');
 
-const path = require('path');
-const storage = require('azure-storage');
+// Make your Express server:
+var app = express()
+    .use(SocketIOFileUpload.router)
+    .use(express.static(__dirname + "/public"))
+    .listen(3000);
 
-const blobService = storage.createBlobService();
+const fs = require('fs')
 
-const listContainers = async () => {
-    return new Promise((resolve, reject) => {
-        blobService.listContainersSegmented(null, (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ message: `${data.entries.length} containers`, containers: data.entries });
-            }
-        });
-    });
-};
+const folderName = 'D:/Coding/newFolder'
 
-const createContainer = async (containerName) => {
-    return new Promise((resolve, reject) => {
-        blobService.createContainerIfNotExists(containerName, { publicAccessLevel: 'blob' }, err => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ message: `Container '${containerName}' created` });
-            }
-        });
-    });
-};
+// Start up Socket.IO:
+var io = socketio.listen(app);
+io.sockets.on("connection", function(socket){
+    console.log('connected');
 
-const uploadString = async (containerName, blobName, text) => {
-    return new Promise((resolve, reject) => {
-        blobService.createBlockBlobFromText(containerName, blobName, text, err => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ message: `Text "${text}" is written to blob storage` });
-            }
-        });
-    });
-};
-
-const uploadLocalFile = async (containerName, filePath) => {
-    return new Promise((resolve, reject) => {
-        const fullPath = path.resolve(filePath);
-        const blobName = path.basename(filePath);
-        blobService.createBlockBlobFromLocalFile(containerName, blobName, fullPath, err => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ message: `Local file "${filePath}" is uploaded` });
-            }
-        });
-    });
-};
-
-const listBlobs = async (containerName) => {
-    return new Promise((resolve, reject) => {
-        blobService.listBlobsSegmented(containerName, null, (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ message: `${data.entries.length} blobs in '${containerName}'`, blobs: data.entries });
-            }
-        });
-    });
-};
-
-const downloadBlob = async (containerName, blobName) => {
-    const dowloadFilePath = path.resolve('./' + blobName.replace('.txt', '.downloaded.txt'));
-    return new Promise((resolve, reject) => {
-        blobService.getBlobToText(containerName, blobName, (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ message: `Blob downloaded "${data}"`, text: data });
-            }
-        });
-    });
-};
-
-const deleteBlob = async (containerName, blobName) => {
-    return new Promise((resolve, reject) => {
-        blobService.deleteBlobIfExists(containerName, blobName, err => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ message: `Block blob '${blobName}' deleted` });
-            }
-        });
-    });
-};
-
-const deleteContainer = async (containerName) => {
-    return new Promise((resolve, reject) => {
-        blobService.deleteContainer(containerName, err => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ message: `Container '${containerName}' deleted` });
-            }
-        });
-    });
-};
-
-const execute = async () => {
-
-    const containerName = "demo";
-    const blobName = "quickstart.txt";
-    const content = "hello Blob SDK";
-    const localFilePath = "./readme.md";
-    let response;
-
-    console.log("Containers:");
-    response = await listContainers();
-    response.containers.forEach((container) => console.log(` -  ${container.name}`));
-
-    const containerDoesNotExist = response.containers.findIndex((container) => container.name === containerName) === -1;
-
-    if (containerDoesNotExist) {
-        await createContainer(containerName);
-        console.log(`Container "${containerName}" is created`);
+    try {
+        if (!fs.existsSync(folderName)){
+            const res = fs.mkdirSync(folderName);
+            console.log('res :', res);
+        }
+    } catch (err) {
+        console.error(err)
     }
 
-    await uploadString(containerName, blobName, content);
-    console.log(`Blob "${blobName}" is uploaded`);
+    fs.watch(folderName, (event, filename) => {
+        console.log('event :', event);
+        console.log('filename :', filename);
+    });
 
-    response = await uploadLocalFile(containerName, localFilePath);
-    console.log(response.message);
+    // Make an instance of SocketIOFileUpload and listen on this socket:
+    var uploader = new SocketIOFileUpload();
 
-    console.log(`Blobs in "${containerName}" container:`);
-    response = await listBlobs(containerName);
-    response.blobs.forEach((blob) => console.log(` - ${blob.name}`));
+    uploader.dir = "D:/Coding/newFolder/";
+    uploader.listen(socket);
 
-    response = await downloadBlob(containerName, blobName);
-    console.log(`Downloaded blob content: ${response.text}"`);
+    // Do something when a file is saved:
+    uploader.on("saved", function(event){
+        console.log('file saved', event.file);
+    });
 
-    await deleteBlob(containerName, blobName);
-    console.log(`Blob "${blobName}" is deleted`);
-
-    await deleteContainer(containerName);
-    console.log(`Container "${containerName}" is deleted`);
-
-}
-
-execute().then(() => console.log("Done")).catch((e) => console.log(e));
+    // Error handler:
+    uploader.on("error", function(event){
+        console.log("Error from uploader", event);
+    });
+});
